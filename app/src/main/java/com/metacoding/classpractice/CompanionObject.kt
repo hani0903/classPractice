561d91a1b275e4ab0b209c8f7dcd15c18048950e
Hani <jhe93@naver.com>
2022-09-24 10:59:46 +0900

    object 키워드

--- app/src/main/java/com/metacoding/classpractice/CompanionObject.kt
+++ app/src/main/java/com/metacoding/classpractice/CompanionObject.kt
@@ -7,7 +7,6 @@ class Book private constructor(val id :Int, val name : String){
     }
 
 }
-
 //object 키워드를 써서 클래스 만들기
 object CarFactory{// 이 객체는 다른 클래스와 다르게 모든 앱을 실행할 때 딱 한번 만들어준다. -> singleton Pattern -> 생성자 없이 직접 호출 가능하다.
 
